aec1187c900d0e6adcee333acaa2f63c157ab110
JayPatel1314 <69890535+JayPatel1314@users.noreply.github.com>
2020-08-29 10:05:50 +0530

    Update __main__.py

--- alluka/__main__.py
+++ alluka/__main__.py
@@ -130,7 +130,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             send_start(bot, update)
     else:
-        update.effective_message.reply_text("Yo, whadup? 🙂")
+        update.effective_message.reply_text("hello! How are you? I will help you.... 🙂🙂🙂🙂")
 
 def send_start(bot, update):
     #Try to remove old message
